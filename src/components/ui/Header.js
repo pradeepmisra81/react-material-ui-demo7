433d7189fb033334dfd1cce9e32c41c6c9e81ab4
pradeep <pradeep.misra@gmail.com>
2022-03-16 15:47:26 +0530

    Updated the Header title

---
+++ src/components/ui/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+
+
+
+const Header = (props) => {
+    return (
+        <AppBar>
+            <Toolbar>
+                dApps Development
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
