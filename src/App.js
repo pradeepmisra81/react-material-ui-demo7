15f621cd08733befdf5529bfcc00bf5786a0377f
pradeep <pradeep.misra@gmail.com>
2022-03-16 14:43:25 +0530

    Added Appbar using MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { Button } from '@material-ui/core';
-import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
     <div className="App">
-      <Button>Click me!</Button>
+      <AppBar position="static">
+      <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
